a0bdbc7dea4f8366cfc0aaf70a071cad7f18d6d9
hvu160699 <hvu160699@gmail.com>
2019-08-05 11:05:36 +0700

    test

--- routes/categories.js
+++ routes/categories.js
@@ -99,7 +99,7 @@ router.post('/api/add', (req, res, docs) => {
 
 
 router.put('/listAllCategory/update/:id', (req, res) => {
-  Product.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, data) => {
+  Product.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, data) => {
     if (!err) {
       res.json(data)
       console.log(data);
